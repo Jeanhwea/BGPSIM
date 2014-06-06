dbb242841411e6f3650792f0dff6334d8b7f6420
胡京徽 <hujh@act.buaa.edu.cn>
2014-06-07 01:35:19 +0800

    buffer operation

--- src/Buffer.h
+++ src/Buffer.h
@@ -2,13 +2,32 @@
 
 #define BUFFER_3NKWNENG
 
+#include "global.h"
+
+using namespace std;
+
 class Buffer {
     private:
+        static deque<Buffer *> mqBuffer;
 
     public:
-        Buffer ();
-        virtual ~Buffer ();
-    
+        u_char    * buf;
+        ssize_t     size;
+        ssize_t     wpos;
+        ssize_t     rpos;
+        sockfd      sfd;
+
+        Buffer();
+        Buffer(ssize_t len);
+        virtual ~Buffer();
+        bool Add(void * data, ssize_t len);
+        u_char * Reserve(ssize_t len);
+        bool Close();
+        bool Write();
+        bool Write(sockfd sfd, Buffer * buf);
+
+        void BufDeque();
+        void BufEnque(Buffer *);
 };
 
 #endif /* end of include guard: BUFFER_3NKWNENG */
